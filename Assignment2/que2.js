88b33c0c76d1cc8369cbca2b65bf85af02e2505a
YanshiAgarwal <aparul742@gmail.com>
2025-11-12 10:24:11 +0530

    initial commit

---
+++ Assignment2/que2.js
@@ -0,0 +1,24 @@
+// Q2. String Manipulation Report
+
+let productName = " wireless headphones PRO ";
+
+// trim and lowercase
+let formatted = productName.trim().toLowerCase();
+
+//first letter capital
+let words = formatted.split(" ");
+for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 0) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+}
+
+// join 
+let finalTitle = words.join(" ");
+
+// replace "Pro" with "Pro Edition"
+finalTitle = finalTitle.replace("Pro", "Pro Edition");
+
+// output
+console.log("Cleaned Title:", finalTitle);
+console.log("Title Length:", finalTitle.length);
---
+++ Assignment2/que3.js
@@ -0,0 +1,19 @@
+// Q3. Math Utility Dashboard
+
+let x = 16.75;
+
+let roundedValue = Math.round(x);
+let squareRoot = Math.sqrt(x);
+let powerValue = Math.pow(x, 3);
+let randomNumber = Math.floor(Math.random() * 41) + 10; // random between 10 and 50
+
+// display result 
+console.log(`Number: ${x}`);
+console.log(`Rounded Value: ${roundedValue}`);
+console.log(`Square Root: ${squareRoot}`);
+console.log(`Power (x^3): ${powerValue}`);
+console.log(`Random Number (10-50): ${randomNumber}`);
+
+console.log(`\n--- Summary ---`);
+console.log(`For x = ${x}, the rounded value is ${roundedValue}, the square root is ${squareRoot.toFixed(3)}, 
+and x³ equals ${powerValue}. A random number between 10 and 50 is ${randomNumber}.`);
